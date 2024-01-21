6b11402e1dc04dc513a0e04a57fdf99d62c0d8c6
astianmuchui <astianmuchui@gmail.com>
2024-01-21 14:06:21 +0300

    Added Table create , alter and drop

--- src/core.php
+++ src/core.php
@@ -30,12 +30,13 @@ interface PixelSequelORM
     public static function Insert(mixed $table, array $data): bool;
     public static function Update(mixed $table, mixed $param_t = "id", mixed $param_n, array $data): bool;
     public static function All(mixed $table, iterable $where=null, iterable $where_like=null, mixed $order_by="", mixed $order="", int $limit=null, bool $json=false): mixed;
-    public function Select(mixed $table, array $rows,mixed $order_by="", mixed $order, mixed $where=null, mixed $where_like=null, int $limit=null): array | object;
     public static function Find(mixed $table, mixed $param_t="id", mixed $param_n, mixed $order_by = "",string $order=""): array;
     public static function Search(mixed $table, mixed $param_t="id", mixed $param_n, mixed $order_by = "", mixed $order=""): array;
+    public static function TableExists(string $table): bool;
     public static function Delete(mixed $table, mixed $param_t="id", mixed $param_n): bool;
     public static function DeleteAll(mixed $table): bool;
     public static function Disconnect(): void;
+
 }
 
 class Model implements PixelSequelORM
@@ -387,6 +388,20 @@ class Model implements PixelSequelORM
     }
 
     /**
+     * @TableExists: check if table exists
+     * @param string $table: table name
+     * @return bool
+     */
+
+     public static function TableExists(string $table): bool
+     {
+         $sql = "SHOW TABLES LIKE '$table'";
+         $stmt = self::$connection->query($sql);
+         $stmt->execute();
+         return $stmt->rowCount() > 0;
+     }
+
+    /**
      * @Delete: delete record from table
      * @param string $table: table name
      * @param string $param_t: parameter type
@@ -429,5 +444,147 @@ class Model implements PixelSequelORM
     }
 }
 
+interface PixelSequelSchema
+{
+    public static function Create(mixed $table, array $data): bool;
+    public static function Drop(mixed $table): bool;
+    public static function Alter(string $table, mixed $column, mixed $set): bool;
+}
+
+class Schema extends Model implements PixelSequelSchema
+{
+
+    /**
+     * @__construct: constructor function
+     * @param void
+     * @return void
+    */
+
+    public function __construct()
+    {
+        if (!(Model::$Connected instanceof true))
+        {
+            die ("Error: Database not connected");
+        }
+    }
+
+    /**
+    * @Create: create table
+    * @param mixed $table: table name
+    * @param array $data: nested array of columns with their properties
+    * @return bool
+    */
+
+
+    public static function Create(mixed $table, array $data = [[]]): bool
+    {
+
+        if (Model::TableExists($table))
+        {
+            exit();
+        }
+
+        $sql = "CREATE TABLE IF NOT EXISTS `$table` (";
+
+        $primaryKey = (null);
+
+        foreach ($data as $col => $properties)
+        {
+            if ($col !== array_key_first($data))
+            {
+                $sql .= ", ";
+            }
+
+            $sql .= "`$col`";
+
+            foreach ($properties as $property => $value)
+            {
+                $property = strtoupper($property);
+
+                switch ($property)
+                {
+                    case 'TYPE':
+                        $sql .= " $value";
+                        break;
+                    case 'LENGTH':
+                        if ($value)
+                        {
+                            $sql .= "($value)";
+                        }
+                        break;
+                    case 'AUTO_INCREMENT':
+                        if ($value)
+                        {
+                            $primaryKey = $col;
+                        }
+                        break;
+                    case 'DEFAULT':
+                        $sql .= " DEFAULT $value";
+                        break;
+                    case 'NULL':
+                        if (!$value)
+                        {
+                            $sql .= " NOT NULL";
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        $sql .= ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+
+        $stmt = Model::$connection->prepare($sql);
+        $stmt->execute();
+
+        /** Alter tables to add primary keys if they do exist */
+        if ($primaryKey)
+        {
+            $alterSql = "ALTER TABLE `$table` ADD PRIMARY KEY (`$primaryKey`);";
+            $alterStmt = Model::$connection->prepare($alterSql);
+            $alterStmt->execute();
+        }
+
+        return true;
+    }
+
+
+    /**
+     * @Drop: drop table
+     * @param string $table: table name
+     * @return bool
+    */
+
+    public static function Drop(mixed $table): bool
+    {
+        $sql = "DROP TABLE `$table`";
+        $stmt = Model::$connection->prepare($sql);
+        return $stmt->execute();
+    }
+
+    /**
+     * @Alter: alter table
+     * @param string $table: table name
+     * @param mixed $column: column to be altered
+     * @param mixed $set: what to set
+     * @return bool
+    */
+
+    public static function Alter(string $table, mixed  $column, mixed $set): bool
+    {
+
+        if (!Model::TableExists($table))
+        {
+            exit();
+        }
+
+        $sql = "ALTER TABLE `$table`  \n \t MODIFY `$column` $set;";
+
+        $stmt = Model::$connection->prepare($sql);
+        return $stmt->execute();
+    }
+
+}
 
 ?>
