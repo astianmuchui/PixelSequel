a22a35fa84061c1b2e9105d24d61fdef857f2f2a
astianmuchui <astianmuchui@gmail.com>
2024-02-07 22:35:15 +0300

    Debugged Model class

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -4,10 +4,6 @@
 namespace PixelSequel\Model;
 
 
-session_start();
-
-
-
 /**
 *
 * @package  PixelSequel ORM
@@ -16,7 +12,7 @@ session_start();
 * @since  1.0.0
 * @license  None
 * @author Sebastian Muchui
-* @subpackage core
+* @subpackage Model
 * @category ORMs
 *
 */
@@ -26,54 +22,6 @@ session_start();
 // ini_set('display_errors', 'On');
 // error_reporting(E_ALL);
 
-class CipherOps
-{
-   public static $method = "AES-128-CTR";
-   public static $options = 0;
-   public static $enc_iv = '1234567891011121';
-
-   public static $key = '$2y$10$Lvh7toMVlSJjwmMHSZ5ULOWkFITbUuK6mr/NG2YKluolXTpI.lLbu';
-   public static $pepper = '$2y$10$np7bVhRUeR5qQNDlAL.hOOvDaEwZdghmLpz8HjkVJnX0vJbmuyto2';
-   public static $salt = '$2y$10$PYbF/lbCcZ5G4wK39svrRO0k2HM/rj.Iu8NqUxpcI01BmfIZq0J9e';
-
-   public static function aes_ctr_ssl_encrypt128( string | array | int $data)
-   {
-        $method = self::$method;
-        $enc_key = self::$key;
-        $options = self::$options;
-        $enc_iv = self::$enc_iv;
-        $iv_length = openssl_cipher_iv_length($method);
-
-        switch(gettype($data))
-        {
-            case "Array":
-                return openssl_encrypt($data,$method,$enc_key,$options,$enc_iv);
-            case "Integer":
-                return openssl_encrypt($data,$method,$enc_key,$options,$enc_iv);
-
-            case "string":
-                return openssl_encrypt($data,$method,$enc_key,$options,$enc_iv);
-        }
-   }
-
-   public static function aes_ctr_ssl_decrypt128( string | array | int $data)
-   {
-        $method = self::$method;
-        $enc_key = self::$key;
-        $options = self::$options;
-        $enc_iv = self::$enc_iv;
-
-        switch(gettype($data))
-        {
-            case "Array":
-                return openssl_decrypt($data,$method,$enc_key,$options,$enc_iv);
-            case "Integer":
-                return openssl_decrypt($data,$method,$enc_key,$options,$enc_iv);
-            case "string":
-                return openssl_decrypt($data,$method,$enc_key,$options,$enc_iv);
-        }
-   }
-}
 
 
 use PDO, PDOException, PDOStatement;
@@ -81,6 +29,8 @@ use PDO, PDOException, PDOStatement;
 interface PixelSequelORM
 {
     public function connect(): PDO | bool;
+    public static function Connection(): PDO;
+    public static function Connected(): bool;
     public static function Insert(mixed $table, array $data): bool;
     public static function Update(mixed $table,  mixed $param_n, array $data, mixed $param_t = "id"): bool;
     public static function All(mixed $table, iterable $where=null, iterable $where_like=null, mixed $order_by="", mixed $order="", int $limit=null, bool $json=false): mixed;
@@ -122,17 +72,12 @@ class Model implements PixelSequelORM
      * @defaults: 
     */
 
-    public function __construct( mixed $db , mixed $uname = "root", mixed $pwd = "", mixed $host = "localhost" )
+    public function __construct( mixed $dbname , mixed $username = "root", mixed $password = "", mixed $dbhost = "localhost" )
     {
-        $_SESSION['uname'] = $this->uname = $uname;
-        $_SESSION['pwd'] = $this->pwd = $pwd;
-        $_SESSION['host'] = $this->host = $host;
-        $_SESSION['db'] = $this->db = $db;
-
-        $_SESSION['uname'] = CipherOps::aes_ctr_ssl_encrypt128($_SESSION['uname']);
-        $_SESSION['pwd'] = CipherOps::aes_ctr_ssl_encrypt128($_SESSION['pwd']);
-        $_SESSION['host'] = CipherOps::aes_ctr_ssl_encrypt128($_SESSION['host']);
-        $_SESSION['db'] = CipherOps::aes_ctr_ssl_encrypt128($_SESSION['db']);
+        $this->uname = $username;
+        $this->pwd = $password;
+        $this->host = $dbhost;
+        $this->db = $dbname;
 
 
         if (!(self::$Connected instanceof true))
@@ -165,6 +110,28 @@ class Model implements PixelSequelORM
         {
             echo "Connection failed: " . $e->getMessage();
         }
+    }
+
+    /**
+     * @Connection: get connection
+     * @param void
+     * @return PDO
+     */
+
+    public static function Connection(): PDO
+    {
+        return self::$connection;
+    }
+
+    /**
+     * @Connected: check if connected
+     * @param void
+     * @return bool
+     */
+
+    public static function Connected(): bool
+    {
+        return self::$Connected;
     }
 
     /**
